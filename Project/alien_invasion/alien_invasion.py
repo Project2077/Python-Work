3d252a41c11ec6ce16c77822e852638f5b34d8d2
Project2077 <Wryyyyyyyyyy@protonmail.com>
2022-08-09 21:43:08 +0800

    add ship and refactoring

--- Project/alien_invasion/alien_invasion.py
+++ Project/alien_invasion/alien_invasion.py
@@ -6,6 +6,8 @@ import pygame
 # 出时，我们将使用模块sys 中的工具来退出游戏。
 from settings import Settings
 
+from ship import Ship
+
 
 class AlienInvasion:
     """管理游戏资源和行为的类"""
@@ -32,17 +34,16 @@ class AlienInvasion:
 
         # # 设置背景色
         # self.bg_color = ((230, 230, 230))
+        self.ship = Ship(self)
 
     def run_game(self):
         """开始游戏的主循环"""
         while True:
             # 监视键盘和鼠标事件。
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
 
             # 每次循环时都重绘屏幕。
-            self.screen.fill(self.settings.bg_color)
+            self._update_screen()
 
             # 让最近绘制的屏幕可见。
             pygame.display.flip()
@@ -62,6 +63,17 @@ class AlienInvasion:
     # 这里，它在每次执行while 循环时都绘制一个空屏幕，并擦去旧屏幕，使得只有新屏幕可见。
     # 我们移动游戏元素时，pygame.display.flip() 将不断更新屏幕，
     # 以显示元素的新位置，并且在原来的位置隐藏元素，从而营造平滑移动的效果。    刷新画面帧
+
+    def _check_events(self):
+        """响应按键和鼠标事件。"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        """更新屏幕上的图像，并切换到新屏幕。"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
 
 
 if __name__ == '__main__':
@@ -83,3 +95,21 @@ if __name__ == '__main__':
 # 颜色值(230, 230, 230)中，红色、绿色和蓝色的量相同，它生成一种浅灰色。我们
 # 将这种颜色赋给了self.bg_color
 # 调用方法fill() 用这种背景色填充屏幕。方法fill() 用于处理surface，只接受一个实参：一种颜色。
+
+
+# 重构：方法_check_events() 和__update_screen()
+# 在大型项目中，经常需要在添加新代码前重构既有代码。重构旨在简化既有代码的
+# 结构，使其更容易扩展。本节将把越来越长的方法run_game() 拆分成两个辅助方
+# 法（helper method）。辅助方法 在类中执行任务，但并非是通过实例调用的。在Python中，辅助方法的名称以单个下划线打头。
+
+# 把管理事件的代码移到一个名为_check_events() 的方法中，以简化
+# run_game() 并隔离事件管理循环。通过隔离事件循环，可将事件管理与游戏的其
+# 他方面（如更新屏幕）分离。
+
+# 为进一步简化run_game() ，将更新屏幕的代码移到一个名为
+# _update_screen() 的方法中
+
+# 如果你开发过大量的游戏，可能早就开始像这样将代码放到不同的方法中了。不过
+# 如果你从未开发过这样的项目，可能不知道如何组织代码。这里采用的做法是，先
+# 编写可行的代码，等代码越来越复杂时再进行重构，以向你展示真正的开发过程：
+# 先编写尽可能简单的代码，等项目越来越复杂后对其进行重构。  对代码进行重构使其更容易扩展后，可以开始处理游戏的动态方面了！
---
+++ Project/alien_invasion/ship.py
@@ -0,0 +1,78 @@
+# 添加飞船图像
+
+# 下面将飞船加入游戏中。为了在屏幕上绘制玩家的飞船，我们将加载一幅图像，再
+# 使用Pygame方法blit() 绘制它。
+
+# 为游戏选择素材时，务必要注意许可。最安全、最不费钱的方式是使用Pixabay等网
+# 站提供的免费图形，无须授权许可即可使用并修改。
+# 在游戏中几乎可以使用任何类型的图像文件，但使用位图（.bmp）文件最为简单，
+# 因为Pygame默认加载位图。虽然可配置Pygame以使用其他文件类型，但有些文件类
+# 型要求你在计算机上安装相应的图像库。大多数图像为.jpg、.png或.gif格式，但
+# 可使用Photoshop、GIMP和Paint等工具将其转换为位图。
+
+# 选择图像时，要特别注意背景色。请尽可能选择背景为透明或纯色的图像，便于使
+# 用图像编辑器将其背景替换为任意颜色。图像的背景色与游戏的背景色匹配时，游
+# 戏看起来最漂亮。你也可以将游戏的背景色设置成图像的背景色。
+
+# 就游戏《外星人入侵》而言，可使用文件ship.bmp，该文件可在
+# 本书主页（ituring.cn/book/2784）的“随书下载”中找到。这个文件的背景色与
+# 项目使用的设置相同。请在项目文件夹（alien_invasion）中新建一个名为images
+# 的文件夹，并将文件ship.bmp保存在其中。
+
+# 创建Ship 类
+
+import pygame
+
+
+class Ship:
+    """管理飞船的类"""
+
+    def __init__(self, ai_game):
+        """初始化飞船并设置其初始位置。"""
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # 加载飞船图像并获取其外接矩形。
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+
+        # 对于每艘新飞船，都将其放在屏幕底部的中央。
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        """在指定位置绘制飞船。"""
+        self.screen.blit(self.image, self.rect)
+
+# Pygame之所以高效，是因为它让你能够像处理矩形（rect 对象）一样处理所有的
+# 游戏元素，即便其形状并非矩形。像处理矩形一样处理游戏元素之所以高效，是因
+# 为矩形是简单的几何形状。例如，通过将游戏元素视为矩形，Pygame能够更快地判
+# 断出它们是否发生了碰撞。这种做法的效果通常很好，游戏玩家几乎注意不到我们
+# 处理的并不是游戏元素的实际形状。在这个类中，我们将把飞船和屏幕作为矩形进行处理。
+
+# Ship 的方法__init__() 接受两个参
+# 数：引用self 和指向当前AlienInvasion 实例的引用。这让Ship 能够访问
+# AlienInvasion 中定义的所有游戏资源。
+# self.screen = ai_game.screen 将屏幕赋给了Ship 的一个属性，以便在这个类的所有方法中轻松访问。
+
+# self.screen_rect = ai_game.screen.get_rect() 使用方法get_rect() 访问屏
+# 幕的属性rect ，并将其赋给了self.screen_rect ，这让我们能够将飞船放到屏幕的正确位置。
+# 调用pygame.image.load() 加载图像，并将飞船图像的位置传递给它
+# 该函数返回一个表示飞船的surface，而我们将这个surface赋给了self.image 。
+# 加载图像后，使用get_rect() 获取相应surface的属性rect ，以便后面能够使用它来指定飞船的位置。
+
+# 处理rect 对象时，可使用矩形四角和中心的 坐标和 坐标。可通过设置这些值来
+# 指定矩形的位置。要让游戏元素居中，可设置相应rect 对象的属性center 、
+# centerx 或centery ；要让游戏元素与屏幕边缘对齐，可使用属性top 、
+# bottom 、left 或right 。除此之外，还有一些组合属性，如midbottom 、
+# midtop 、midleft 和midright 。要调整游戏元素的水平或垂直位置，可使用
+# 属性x 和y ，分别是相应矩形左上角的 坐标和 坐标。这些属性让你无须做游戏
+# 开发人员原本需要手工完成的计算，因此会经常用到。
+
+# 注意 　在Pygame中，原点(0, 0)位于屏幕左上角，向右下方移动时，坐标值将
+# 增大。在1200 × 800的屏幕上，原点位于左上角，而右下角的坐标为(1200,
+# 800)。这些坐标对应的是游戏窗口，而不是物理屏幕。
+
+# 在屏幕上绘制飞船
+# 更新alien_invasion.py，创建一艘飞船并调用其方法blitme()
+
+

dc42eb077a444577cdf49d2861c3efb5a0c86ab7
Project2077 <Wryyyyyyyyyy@protonmail.com>
2022-08-14 13:09:51 +0800

    copy 20

--- Project/web_applications/learning_log/learning_logs/views.py
+++ Project/web_applications/learning_log/learning_logs/views.py
@@ -1,35 +1,26 @@
 from django.shortcuts import render, redirect
-from .models import Topic, Entry
-from .forms import TopicForm, EntryForm
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
 
-
-# Create your views here.
-
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
 
 def index(request):
-    """学习笔记的主页。"""
+    """The home page for Learning Log."""
     return render(request, 'learning_logs/index.html')
-
 
 @login_required
 def topics(request):
-    """显示所有的主题。"""
+    """Show all topics."""
     topics = Topic.objects.filter(owner=request.user).order_by('date_added')
-
-
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
-
 @login_required
 def topic(request, topic_id):
-    """显示单个主题及其所有的条目。"""
+    """Show a single topic and all its entries."""
     topic = Topic.objects.get(id=topic_id)
-
-
-    # 确认请求的主题属于当前用户。
+    # Make sure the topic belongs to the current user.
     if topic.owner != request.user:
         raise Http404
 
@@ -37,69 +28,63 @@ def topic(request, topic_id):
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
-
 @login_required
 def new_topic(request):
-    """添加新主题。"""
+    """Add a new topic."""
     if request.method != 'POST':
-        # 未提交数据：创建一个新表单。
+        # No data submitted; create a blank form.
         form = TopicForm()
     else:
-        # POST提交的数据：对数据进行处理。
+        # POST data submitted; process data.
         form = TopicForm(data=request.POST)
-    if form.is_valid():
-        new_topic = form.save(commit=False)
-        new_topic.owner = request.user
-        new_topic.save()
-        return redirect('learning_logs:topics')
+        if form.is_valid():
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
+            return redirect('learning_logs:topics')
 
-    # 显示空表单或指出表单数据无效。
+    # Display a blank or invalid form.
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
-
 @login_required
 def new_entry(request, topic_id):
-    """在特定主题中添加新条目。"""
+    """Add a new entry for a particular topic."""
     topic = Topic.objects.get(id=topic_id)
+    
     if request.method != 'POST':
-        # 未提交数据：创建一个空表单。
+        # No data submitted; create a blank form.
         form = EntryForm()
     else:
-        # POST提交的数据：对数据进行处理。
+        # POST data submitted; process data.
         form = EntryForm(data=request.POST)
-    if form.is_valid():
-        new_entry = form.save(commit=False)
-        new_entry.topic = topic
-        new_entry.save()
-        return redirect('learning_logs:topic', topic_id=topic_id)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
 
-    # 显示空表单或指出表单数据无效。
+    # Display a blank or invalid form.
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
 
-
 @login_required
 def edit_entry(request, entry_id):
-    """编辑既有条目。"""
-
+    """Edit an existing entry."""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
-
     if topic.owner != request.user:
         raise Http404
-
+    
     if request.method != 'POST':
-        # 初次请求：使用当前条目填充表单。
+        # Initial request; pre-fill form with the current entry.
         form = EntryForm(instance=entry)
     else:
-        # POST提交的数据：对数据进行处理。
+        # POST data submitted; process data.
         form = EntryForm(instance=entry, data=request.POST)
         if form.is_valid():
             form.save()
-            return redirect('learning_logs:topic',
-                            topic_id=topic.id)
+            return redirect('learning_logs:topic', topic_id=topic.id)
 
     context = {'entry': entry, 'topic': topic, 'form': form}
-    return render(request, 'learning_logs/edit_entry.html',
-                  context)
+    return render(request, 'learning_logs/edit_entry.html', context)
